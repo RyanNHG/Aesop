d9f5c2200f5cfa39801f93dd1ed6e7fd89e290bf
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2016-03-06 18:52:13 -0600

    Add gulp clean command to remove all generated files

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var browserify = require('browserify');
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var sass = require('gulp-sass');
+var clean = require('gulp-clean');
 
 var paths = {
     html: './webapp/src/**/*.html',
@@ -41,6 +42,12 @@ gulp.task('watch', function() {
     gulp.watch(paths.sass, ['sass']);
     gulp.watch(paths.js, ['browserify']);
 
+});
+
+gulp.task('clean', function () {
+	gulp.src('./webapp/dist', {read: false})
+  gulp.src('./node_modules', {read: false})
+		.pipe(clean({force: true}));
 });
 
 gulp.task('default', ['watch', 'browserify', 'sass', 'copyResources']);
