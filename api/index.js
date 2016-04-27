eb17bba966ccbb40bf530aa4b6608ef673287ff2
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2016-04-26 22:00:51 -0500

    remove credentials reference

--- api/index.js
+++ api/index.js
@@ -1,15 +1,14 @@
 var userApi = require('./user');
 var fableApi = require('./fable');
 var mongoose = require('mongoose');
-var credentials = require('./credentials');
 
 module.exports = function(app){
 
   const route = '/api';
 
   // Connect to MongoDB
-  console.log('Connecting to MongoLab as ' + credentials.username + '...');
-  var dbUrl = 'mongodb://' + (process.env.MONGO_USER || credentials.username) + ':' + (process.env.MONGO_PWD || credentials.password) + '@ds013918.mlab.com:13918/aesop-db'
+  console.log('Connecting to MongoLab as ' + process.env.MONGO_USER + '...');
+  var dbUrl = 'mongodb://' + (process.env.MONGO_USER) + ':' + (process.env.MONGO_PWD) + '@ds013918.mlab.com:13918/aesop-db'
   mongoose.connect(dbUrl);
   console.log('...connected!');
 
