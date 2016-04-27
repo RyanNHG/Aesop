82d455601c690d03204c23a7f882662385b9ebc1
Ryan Haskell-Glatz <ryan.nhg@gmail.com>
2016-04-26 21:53:17 -0500

    remove src

--- webapp/src/app/app.js
+++
@@ -1,8 +0,0 @@
-angular.module('app', [
-    require('navbar'),
-    require('pages'),
-	require('api-service'),
-    require('angular-route')
-])
-    .config(require('./app.router'))
-;
--- webapp/src/app/pages/welcome-page/index.js
+++
@@ -1,7 +0,0 @@
-angular.module(module.exports = 'welcomePage',[
-	require('shared/sign-in-form')
-])
-  .component(module.exports, {
-    templateUrl: 'templates/pages/welcome-page/welcome-page.tpl.html',
-    controller: require('./welcome-page.ctrl')
-  })
